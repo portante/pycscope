559b5fa30ce607abd0f5be57dd5a437aa1ff2f18
Peter Portante <peter.portante@redhat.com>
2012-03-01 23:31:08 -0500

    Add some spacing for doc strings; put each result on a new line so that it is easier to follow.

--- test/testissues.py
+++ test/testissues.py
@@ -14,28 +14,38 @@ class TestIssues(unittest.TestCase):
         self.fnmbuf = []
 
     def test0018(self,):
-        """Make sure two newlines occur after a file mark
-        when the source file starts with non-symbol text.
+        """ Make sure two newlines occur after a file mark when the source
+            file starts with non-symbol text.
         """
         cwd = os.getcwd()
         fn = "issue0018.py"
         pycscope.parseFile(cwd, fn, self.idxbuf, self.fnmbuf)
         output = "".join(self.idxbuf)
-        self.assertEqual(output, "\n\t@issue0018.py\n\n"
+        self.assertEqual(output, "\n"
+                                 "\t@issue0018.py\n"
+                                 "\n"
                                  "1 import \n"
-                                 "\t~<sys\n\n\n"
+                                 "\t~<sys\n"
+                                 "\n"
+                                 "\n"
                                  "3 \n"
-                                 "a\n= 42 \n\n"
+                                 "a\n"
+                                 "= 42 \n"
+                                 "\n"
                                  "4 \n")
 
     def test0019(self,):
-        """Make sure new lines are observed 
-        when NEWLINE token doesn't exist.
+        """ Make sure new lines are observed when NEWLINE token doesn't exist.
         """
         src = "(a,\nb) = 4, 2\n"
         pycscope.parseSource(src, self.idxbuf)
         output = "".join(self.idxbuf)
-        self.assertEqual(output, "\n\n1 ( \n"
-                                 "a\n, \n\n"
+        self.assertEqual(output, "\n"
+                                 "\n"
+                                 "1 ( \n"
+                                 "a\n"
+                                 ", \n"
+                                 "\n"
                                  "2 \n"
-                                 "b\n) = 4 , 2 \n")
+                                 "b\n"
+                                 ") = 4 , 2 \n")
