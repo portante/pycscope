b00b02fc9647fc88a13b169259a5659103cabce6
Peter Portante <peter.portante@redhat.com>
2012-03-11 17:01:14 -0400

    Thanks to Brett Cannon for pointing out at the PyCon 2012 poster session that we are now working with a concrete syntax tree, and not an abstract syntax tree.

--- replaceNodeType.py
+++ replaceNodeType.py
@@ -4,8 +4,8 @@ nodeNames = token.tok_name
 nodeNames.update(symbol.sym_name)
 
 def replaceNodeType(treeList):
-    """Replaces the 0th element in the list with the name
-    that corresponds to its node value.
+    """ Replaces the 0th element in the list with the name
+        that corresponds to its node value.
     """
     # Replace node num with name
     treeList[0] = nodeNames[treeList[0]]
@@ -15,11 +15,14 @@ def replaceNodeType(treeList):
         if type(treeList[i]) == types.ListType:
             replaceNodeType(treeList[i])
 
-def main(fn="/Users/dwhall/Code/other/pyzeroconf-0.12/Browser.py"):
-    """Returns a readable AST list for the given filename."""
+def main(fn=""):
+    """ Returns a readable (c)oncrete (s)yntax (t)ree (CST)
+        list for the given filename.
+    """
+    if not fn: return []
     src = open(fn,'r').read()
-    ast = parser.suite(src)
-    lis = parser.ast2list(ast, True)
+    cst = parser.suite(src)
+    lis = parser.st2list(cst, True)
     replaceNodeType(lis)
     return lis
 
