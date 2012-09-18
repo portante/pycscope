9309d9b0e17a28381770730372810410848e1b8e
Peter Portante <peter.portante@redhat.com>
2012-09-18 09:17:46 -0400

    Implement dumpCst for tuples to close out Issue #9

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pycscope import __version__
 
 setup(name="pycscope",
-      version="0.3c-pajp",
+      version=__version__,
       description="Generates a cscope index of Python source trees",
       author="Dean Hall",
       author_email="dwhall256\x40yahoo\x2Ecom",
